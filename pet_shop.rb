7e0d3371133ec993d649108595d2079a7dca8db8
onitwin <onitwin@gmail.com>
2020-01-10 12:26:38 +0000

    add remove cash function

--- pet_shop.rb
+++ pet_shop.rb
@@ -7,3 +7,11 @@ end
 def total_cash(shop)
   return shop[:admin][:total_cash]
 end
+
+def add_or_remove_cash(shop,value)
+  shop[:admin][:total_cash] += value
+end
+
+def add_or_remove_cash(shop,value)
+  shop[:admin][:total_cash] += value
+end
