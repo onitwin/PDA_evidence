3f7c9fd39c383780cf376d0bf547ba00954246e5
onitwin <onitwin@gmail.com>
2020-01-11 11:38:19 +0000

    add customer_can_afford_pet function

--- pet_shop.rb
+++ pet_shop.rb
@@ -77,3 +77,14 @@ end
 def add_pet_to_customer(customer, pet)
   customer[:pets].push(pet)
 end
+
+#optional work below
+
+def customer_can_afford_pet(customer,pet)
+  if customer[:cash] >= pet[:price]
+    return true
+  else return false
+  end
+end
+
+#integration tests
